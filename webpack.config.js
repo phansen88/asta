57c8331c28b97dcf2ce7338128bce0a1b04f16ea
Peter Hansen <phansen88@gmail.com>
2019-10-29 15:22:26 +0100

    Major release added bootstrap, postcss

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,32 @@
+// Webpack uses this to work with directories
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
+// This is main configuration object.
+// Here you write different options and tell Webpack what to do
 module.exports = {
-    mode: 'development',
-    entry: [
-        path.resolve(__dirname, './src/sass/main.scss'),
-    ],
+    
+    // Path to your entry point. From this file Webpack will begin his work
+    entry: path.resolve(__dirname, 'src/js/main.js'),
+    
     output: {
-        path: path.resolve(__dirname, './dist/css'),
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
     },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: '/styles.css',
-            ignoreOrder: false, // Enable to remove warnings about conflicting order
-        }),{
-            apply(compiler) {
-              compiler.hooks.shouldEmit.tap('Remove styles from output', (compilation) => {
-                delete compilation.assets['main.js'];  // Remove asset. Name of file depends of your entries and 
-                return true;
-              })
-            }
-          }
-    ],
+    
     module: {
         rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
             {
                 test: /\.(sa|sc|c)ss$/,
                 use: [
@@ -34,21 +37,55 @@ module.exports = {
                         },
                     },
                     'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            config: {
+                                path: path.resolve(__dirname, './'),
+                            }
+                        }
+                    },
                     'sass-loader'
                 ],
             },
             {
-                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                test: /\.(png|jpe?g|gif|svg)$/,
+                exclude: /(sass)/,
                 use: [
-                  {
-                    loader: 'file-loader',
-                    options: {
-                      name: '[name].[ext]',
-                      outputPath: '../fonts'
+                    {
+                        loader: "file-loader",
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: './img',
+                            publicPath: '../img' 
+                        }
                     }
-                  }
                 ]
-              }
-        ],
+            },
+            {
+                test: /\.(woff|woff2|ttf|otf|eot|svg)$/,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: './fonts',
+                            publicPath: '../fonts' 
+                        }
+                    }
+                ]
+            }
+        ]
     },
+
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "/css/styles.css"
+        }),
+        new CopyPlugin([
+          { from: 'src/img', to: 'img' }
+        ]),
+    ],
+    
+    mode: 'development'
 };
